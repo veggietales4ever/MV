4dfb2f88a8bf2116a081dbbef467aedf41c148ba
veggietales4ever <veggietales4ever@gmail.com>
2025-01-23 23:59:10 -0800

    camera updated (follow / panning) as an export

--- control/camera_2d.gd
+++ control/camera_2d.gd
@@ -1,6 +1,8 @@
 extends Camera2D
 
 @export var player = CharacterBody2D
+@export var camera: camera_state
+enum camera_state {FOLLOW, PANNING}
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,15 +11,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	camera_follow()
+	match camera:
+		camera_state.FOLLOW:
+			camera_follow()
+		camera_state.PANNING:
+			camera_panning()
 
 
 func camera_panning():
+	anchor_mode = Camera2D.ANCHOR_MODE_FIXED_TOP_LEFT
 	position = player.global_position
-	#var x = floor(position.x / 576) 
-	#var y = floor(position.y / 324)
+	var x = floor(position.x / 320) * 320 
+	var y = floor(position.y / 180) * 180
 	
-	#position = Vector2(x,y)
+	position = Vector2(x,y)
 
 func camera_follow():
 	anchor_mode = Camera2D.ANCHOR_MODE_DRAG_CENTER
