b7550c9a8f3f656a3ce2d24fbc85e5abe53ae149
veggietales4ever <veggietales4ever@gmail.com>
2025-03-01 22:16:41 -0800

    finally figured it out. would like to have more contorl over the awlk speed when in attack mode but i'm ok with this for now

--- entities/enemy/slime enemy/detection_zone.gd
+++ entities/enemy/slime enemy/detection_zone.gd
@@ -14,15 +14,14 @@ func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	body_exited.connect(_on_body_exited)
 	blackboard = bt_player.blackboard
+	blackboard.bind_var_to_property(BBNames.target_var, self, "target", true)
 	
 	
 # When any node enters, this event triggers. check if character
 func _on_body_entered(p_body : Node2D):
 	if p_body is Character:
 		target = p_body
-		blackboard.bind_var_to_property(BBNames.target_var, self, "target", true)
-		print(BBNames.target_var)
-		print(target, "is in the detection zone")
+		
 		
 func _on_body_exited(p_body : Node2D):
 	if target == p_body:
