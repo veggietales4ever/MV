c46c006d2c93e30ec6c8dc2832455e528cf4bce1
veggietales4ever <veggietales4ever@gmail.com>
2025-03-10 06:49:48 -0700

    can move / can't move during save working

--- entities/objects/interact/savepoint.gd
+++ entities/objects/interact/savepoint.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var anim_player: AnimationPlayer = $AnimationPlayer
 
+var movement : PlayerInput
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	anim_player.play("idle")
@@ -9,8 +11,21 @@ func _ready() -> void:
 
 func _on_body_entered(player : Character) -> void:
 	if player:
+		player_stop()
 		anim_player.play("touch")
 		await anim_player.animation_finished
-		#get_tree().create_timer(2.0).timeout
 		SaveManager.save_game()
+		queue_free()
+		get_tree().create_timer(2.0).timeout
 		print("Game Saved")
+		player_start()
+
+func player_stop() -> void:
+	PlayerManager.player.can_move = false
+	PlayerManager.player.input_handler.can_move = false
+	PlayerManager.player.input_handler.reset_input_states()
+	
+func player_start() -> void:
+	PlayerManager.player.can_move = true
+	PlayerManager.player.input_handler.can_move = true
+	#PlayerManager.player.input_handler.reset_input_states()
